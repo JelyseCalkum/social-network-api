2384e3bae9043ddef758d8188be4d29bf8adf6f2
jelysecalkum <jelyse.lauren@gmail.com>
2023-03-18 14:10:06 -0700

    fixed thought routes to working code

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,29 +1,32 @@
+// Require express router
 const router = require('express').Router();
 
-const {
-    getAllThoughts,
-    getThoughtsById,
-    createThoughts,
+// Set requirements (from thoughts-controller)
+const { 
+    getAllThoughts, 
+    getThoughtsById, 
+    createThoughts, 
     updateThoughts,
     deleteThoughts,
     addReaction,
-    deleteReaction,
+    deleteReaction
+
 } = require('../../controllers/thoughts-controller');
 
-// GET /api/thoughts 
-router.route('/').get(getAllThoughts)//.post(addThought)
-;
-// GET, PUT, DELETE /api/thoughts/:id
+// -- Directs to: /api/thoughts <GET>
+router.route('/').get(getAllThoughts);
+
+// -- Directs to: /api/thoughts/:id <GET, PUT, DELETE>
 router.route('/:id').get(getThoughtsById).put(updateThoughts).delete(deleteThoughts); 
 
-// POST /api/thoughts/:userId
-router.route('/userId').post(createThoughts);
+// -- Directs to: /api/thoughts/:userId <POST>
+router.route('/:userId').post(createThoughts);
 
-// POST /api/thoughts/:thoughtId/reactions
+// -- Directs to: /api/thoughts/:thoughtId/reactions <POST>
 router.route('/:thoughtId/reactions').post(addReaction);
 
-// DELETE /api/thoughts/:thoughtId/reactionId
-router.route('/:thoughtId/reactions/:reactionid').delete(deleteReaction);
+// -- Directs to: /api/thoughts/:thoughtId/reactionId <DELETE>
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
-
+// Export module router
 module.exports = router;
