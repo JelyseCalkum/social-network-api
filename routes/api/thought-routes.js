d57815f48ab144e521960b6fd38c962d10c4faaf
jelysecalkum <jelyse.lauren@gmail.com>
2023-03-18 17:31:52 -0700

    final push

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -13,19 +13,19 @@ const {
 
 } = require('../../controllers/thoughts-controller');
 
-// -- Directs to: /api/thoughts <GET>
+// GET /api/thoughts 
 router.route('/').get(getAllThoughts);
 
-// -- Directs to: /api/thoughts/:id <GET, PUT, DELETE>
+// GET, PUT, DELETE /api/thoughts/:userid 
 router.route('/:id').get(getThoughtsById).put(updateThoughts).delete(deleteThoughts); 
 
-// -- Directs to: /api/thoughts/:userId <POST>
+// POST /api/thoughts/:userId 
 router.route('/:userId').post(createThoughts);
 
-// -- Directs to: /api/thoughts/:thoughtId/reactions <POST>
+// POST /api/thoughts/:thoughtId/reactions 
 router.route('/:thoughtId/reactions').post(addReaction);
 
-// -- Directs to: /api/thoughts/:thoughtId/reactionId <DELETE>
+// DELETE reaction /api/thoughts/thoughtID/reactions/reactionID
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 // Export module router
